8a3b60f3fd3d68de304ae4d56de449dc2d2dc357
proandceo <proandceo@gmail.com>
2022-10-12 22:09:56 +1000

    repository 테스트 환경 세팅 완료

--- src/test/java/com/example/junitproject/domain/BookRepositoryTest.java
+++ src/test/java/com/example/junitproject/domain/BookRepositoryTest.java
@@ -1,4 +1,22 @@
 package com.example.junitproject.domain;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest //DB와 관련된 컴포넌트만 메모리에 로딩
 public class BookRepositoryTest {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    // 1. 책 등록
+    @Test
+    public void saveTest(){
+        System.out.println("책 등록 test");
+    }
+    // 2. 책 목록 보기
+    // 3. 책 한건 보기
+    // 4. 책 수정
+    // 5. 책 삭제
 }
