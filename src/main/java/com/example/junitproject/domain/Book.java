c21f84a72adfc788c6d6f68ed007d12b86df76f2
proandceo <proandceo@gmail.com>
2022-10-12 15:12:38 +1000

    Book 엔티티 생성 완료

--- src/main/java/com/example/junitproject/domain/Book.java
+++ src/main/java/com/example/junitproject/domain/Book.java
@@ -1,4 +1,27 @@
 package com.example.junitproject.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 50, nullable = false)
+    private String title;
+
+    @Column(length = 20, nullable = false)
+    private String author;
+
+
 }
