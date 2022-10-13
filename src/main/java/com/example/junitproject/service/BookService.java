f8ce742aebe25db7c4b4150d11df1910bf10680a
proandceo <proandceo@gmail.com>
2022-10-13 19:23:33 +1000

    develop save

--- src/main/java/com/example/junitproject/service/BookService.java
+++ src/main/java/com/example/junitproject/service/BookService.java
@@ -1,7 +1,21 @@
 package com.example.junitproject.service;
 
+import com.example.junitproject.domain.Book;
+import com.example.junitproject.domain.BookRepository;
+import com.example.junitproject.web.dto.BookRespDto;
+import com.example.junitproject.web.dto.BookSaveReqDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@RequiredArgsConstructor
 @Service
 public class BookService {
+    private final BookRepository bookRepository;
+
+    @Transactional(rollbackFor = RuntimeException.class)
+    public BookRespDto saveBooks(BookSaveReqDto dto) {
+        Book bookPS = bookRepository.save(dto.toEntity());
+        return new BookRespDto().toDto(bookPS);
+    }
 }
--- src/main/java/com/example/junitproject/web/dto/BookRespDto.java
+++ src/main/java/com/example/junitproject/web/dto/BookRespDto.java
@@ -1,4 +1,20 @@
 package com.example.junitproject.web.dto;
 
+import com.example.junitproject.domain.Book;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class BookRespDto {
+    private Long id;
+    private String title;
+    private String author;
+
+    public BookRespDto toDto(Book bookPS){
+        this.id = bookPS.getId();
+        this.title = bookPS.getTitle();
+        this.author = bookPS.getAuthor();
+        return this;
+    }
 }
