9b180025313c5c3f0373ab0b8a91e74245cfb75a
proandceo <proandceo@gmail.com>
2022-10-13 20:11:17 +1000

    build update

--- src/main/java/com/example/junitproject/service/BookService.java
+++ src/main/java/com/example/junitproject/service/BookService.java
@@ -5,6 +5,7 @@ import com.example.junitproject.domain.BookRepository;
 import com.example.junitproject.web.dto.BookRespDto;
 import com.example.junitproject.web.dto.BookSaveReqDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.Mapping;
@@ -37,5 +38,10 @@ public class BookService {
         } else {
             throw new RuntimeException("해당 아이디를 찾을 수 없습니다.");
         }
+    }
+
+    @Transactional(rollbackFor = RuntimeException.class)
+    public void deleteBook(Long id) {
+        bookRepository.deleteById(id);
     }
 }
